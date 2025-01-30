91698f40300b4152c58a8d34d1e972bb11bf2456
Kevin Fitahiantsoa Ralison <kralison@c1r4s11.42antananarivo.mg>
2025-01-30 07:53:14 +0300

    almost finished training

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: kralison <kralison@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 11:56:00 by kralison          #+#    #+#             */
-/*   Updated: 2025/01/27 14:49:17 by kralison         ###   ########.fr       */
+/*   Updated: 2025/01/29 20:05:43 by kralison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@
 
 # define WIDTH 400
 # define HEIGHT 200
+# define KEY_PRESSED 2
+# define DESTROY 17
+# define W_KEY 119
+# define S_KEY 115
+# define A_KEY 97
+# define D_KEY 100
+# define LEFT_KEY 65361
+# define RIGHT_KEY 65363
+# define ESC_KEY 65307
+
 
 typedef struct	s_win
 {
@@ -39,9 +49,13 @@ typedef struct	s_program
 {
 	void	*mlx;
 	t_win	win;
+	int		x;
+	int		y;
 }				t_program;
 
+void	init_program(t_program *p);
+int		program_finish(t_program *p);
 void	window_img_pixel(t_win *win, int x, int y, int color);
-int		rgb(int r, int g, int b);
+int		handle_key(int keycode, t_program *p);
 
 #endif
--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: kralison <kralison@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:04:14 by kralison          #+#    #+#             */
-/*   Updated: 2025/01/27 15:19:57 by kralison         ###   ########.fr       */
+/*   Updated: 2025/01/30 07:52:49 by kralison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,15 +50,27 @@ int	get_b(int color)
 	return (color & 0xFF);
 }
 
-int	gradient(int x, int color1, int color2)
+int	shade(double mult, int color)
+{
+	int r;
+	int	g;
+	int	b;
+
+	r = get_r(color) * (1 - mult);
+	g = get_g(color) * (1 - mult);
+	b = get_b(color) * (1 - mult);
+	return (rgb(r, g, b));
+}
+
+int	gradient(int min, int max, int pos, int color1, int color2)
 {
 	int	r;
 	int	g;
 	int	b;
 
-	r = (get_r(color1) + get_r(color2)) * x / WIDTH;
-	g = (get_g(color1) + get_g(color2)) * x / WIDTH;
-	b = (get_b(color1) + get_b(color2)) * x / WIDTH;
+	r = get_r(color1) * (max - pos) / max + get_r(color2) * pos / max + min;
+	g = get_g(color1) * (max - pos) / max + get_g(color2) * pos / max + min;
+	b = get_b(color1) * (max - pos) / max + get_b(color2) * pos / max + min;
 	return (rgb(r, g, b));
 }
 
@@ -76,34 +88,53 @@ int	invert(int rgb)
 
 void	init_program(t_program *p)
 {
+	p->win.win = NULL;
+	p->win.img = NULL;
 	p->mlx = mlx_init();
+	if (p->mlx == NULL)
+		program_finish(p);
 	p->win.win = mlx_new_window(p->mlx, WIDTH, HEIGHT, "CUB3D");
+	if (p->win.win == NULL)
+		program_finish(p);
 	p->win.img = mlx_new_image(p->mlx, WIDTH, HEIGHT);
+	if (p->win.img == NULL)
+		program_finish(p);
 	p->win.addr = mlx_get_data_addr(p->win.img, &p->win.bpp,
 			&p->win.ln, &p->win.ed);
+}
+
+int	program_finish(t_program *p)
+{
+	int	error;
+
+	error = p->mlx == NULL || p->win.win == NULL || p->win.img == NULL;
+	if (p->mlx != NULL && p->win.img != NULL)
+		mlx_destroy_image(p->mlx, p->win.img);
+	if (p->mlx != NULL && p->win.win != NULL)
+		mlx_destroy_window(p->mlx, p->win.win);
+	if (p->mlx != NULL)
+		mlx_destroy_display(p->mlx);
+	if (p->mlx != NULL)
+		free(p->mlx);
+	exit(error);
+	return (0);
+}
+
+int	handle_key(int keycode, t_program *p)
+{
+	if (keycode == ESC_KEY)
+		program_finish(p);
+	return (0);
 }
 
 int	main(void)
 {
 	t_program	p;
-	int			x;
-	int			y;
-	int			color;
 
 	init_program(&p);
-	y = 0;
-	color = rgb(255, 255, 255);
-	while(y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			window_img_pixel(&p.win, x, y, gradient(x, color, invert(color)));
-			x++;
-		}
-		y++;
-	}
 	mlx_put_image_to_window(p.mlx, p.win.win, p.win.img, 0, 0);
+	mlx_hook(p.win.win, DESTROY, 0, program_finish, &p);
+	mlx_hook(p.win.win, KEY_PRESSED, 1L<<0, handle_key, &p);
 	mlx_loop(p.mlx);
 	return (0);
 }
